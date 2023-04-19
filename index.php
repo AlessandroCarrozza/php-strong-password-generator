b0d96ff1dc9924da9c09b65bebab263de317f939
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-04-19 17:36:31 +0200

    Creazione form con get action

--- index.php
+++ index.php
@@ -8,10 +8,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
+
+    <?php
+        $passwordLength = $_GET["passwordLength"];
+    ?>
+
+
     <div class="container">
         <form action="index.php" method="GET" class="mt-5">
             <label for="passwordLength">Lunghezza Password</label>
             <input type="text" id="passwordLength" name="passwordLength">
+
+            <button type="submit" class="btn btn-primary">Invia</button>
+            <input class="btn btn-dark" type="reset" value="Reset">
         </form>
     </div>
 </body>
