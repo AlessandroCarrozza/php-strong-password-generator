503cf3faeb16776374a78ca2d2fc5975cf8ab64d
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-04-19 17:56:34 +0200

    Lavorazione delle function

--- index.php
+++ index.php
@@ -11,6 +11,19 @@
 
     <?php
         $passwordLength = $_GET["passwordLength"];
+
+        $generatedPassword = "";
+
+        $passwordItems = "a b c d e f g h i j k l m n o
+        p q r s t u v w x y z A B C D E F G H I J K L M
+        N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 0 
+        [ ] { } @ # - _";
+
+        function generatePassword() {
+            for ($i = 0; $i < $passwordLength; $i++) { 
+                $passwordArray = explode(" ", $passwordItems);
+            }
+        }
     ?>
 
 
