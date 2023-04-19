0a9e25ee8da9a1dfcfab6dc66c4a4efc9a774433
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-04-19 17:29:32 +0200

    Inizializzazione

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strong Password Generator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <form action="index.php" method="GET" class="mt-5">
+            <label for="passwordLength">Lunghezza Password</label>
+            <input type="text" id="passwordLength" name="passwordLength">
+        </form>
+    </div>
+</body>
+</html>
